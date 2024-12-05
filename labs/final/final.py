2084d19fee67d465c1f29b45742ce3ff84ee4cdd
ethsen <ethansenatore@gmail.com>
2024-12-05 15:33:13 -0500

    Big change

--- labs/final/final.py
+++ labs/final/final.py
@@ -1,32 +1,27 @@
-import sys
 import numpy as np
 from copy import deepcopy
 from math import pi
-
 import rospy
-# Common interfaces for interacting with both the simulation and real environments!
-from core.interfaces import ArmController
-from core.interfaces import ObjectDetector
-
-# for timing that is consistent with simulation or real time as appropriate
-from core.utils import time_in_seconds
-
-
+from core.interfaces import ArmController, ObjectDetector
+from lib.IK_position_null import IK
+from lib.calculateFK import FK
 
 if __name__ == "__main__":
     try:
-        team = rospy.get_param("team") # 'red' or 'blue'
+        team = rospy.get_param("team")  # 'red' or 'blue'
     except KeyError:
         print('Team must be red or blue - make sure you are running final.launch!')
         exit()
 
     rospy.init_node("team_script")
+    arm_controller = ArmController()
+    object_detector = ObjectDetector()
+    inverse_kinematics = IK()
+    forward_kinematics = FK()
 
-    arm = ArmController()
-    detector = ObjectDetector()
-
-    start_position = np.array([-0.01779206, -0.76012354,  0.01978261, -2.34205014, 0.02984053, 1.54119353+pi/2, 0.75344866])
-    arm.safe_move_to_position(start_position) # on your mark!
+    dropoff_position = np.array([0.56, 0.15, 0.24])
+    initial_arm_position = np.array([0, 0, 0, -3.1415 / 2, 0, 3.1415 / 2, 3.1415 / 4])
+    arm_controller.safe_move_to_position(initial_arm_position)  # On your mark!
 
     print("\n****************")
     if team == 'blue':
@@ -34,22 +29,51 @@ if __name__ == "__main__":
     else:
         print("**  RED TEAM  **")
     print("****************")
-    input("\nWaiting for start... Press ENTER to begin!\n") # get set!
-    print("Go!\n") # go!
+    input("\nWaiting for start... Press ENTER to begin!\n")  # Get set!
+    print("Go!\n")  # Go!
 
-    # STUDENT CODE HERE
+    def detect_blocks():
+        detected_objects = object_detector.get_detections()
+        current_transform = forward_kinematics.forward(arm_controller.get_positions())[1]
+        camera_to_world_transform = current_transform @ object_detector.get_H_ee_camera()
+        return [camera_to_world_transform @ pose for _, pose in detected_objects]
 
-    # get the transform from camera to panda_end_effector
-    H_ee_camera = detector.get_H_ee_camera()
+    block_poses = detect_blocks()
+    arm_controller.open_gripper()
 
-    # Detect some blocks...
-    for (name, pose) in detector.get_detections():
-         print(name,'\n',pose)
+    for block_pose in block_poses:
+        arm_controller.open_gripper()
+        block_pose[2,3] += 0.225
+        joint_positions, _, success, message = inverse_kinematics.inverse(block_pose, arm_controller.get_positions(), "J_pseudo", 0.5)
+        if not success:
+            print(f"Failed to compute IK for block pose: {message}")
+            continue
+        arm_controller.safe_move_to_position(joint_positions)
+        block_pose[2,3] -= 0.225
+        joint_positions_lower, _, success, message = inverse_kinematics.inverse(block_pose, joint_positions, "J_pseudo", 0.5)
+        if not success:
+            print(f"Failed to compute IK for lower block pose: {message}")
+            continue
 
-    # Uncomment to get middle camera depth/rgb images
-    # mid_depth = detector.get_mid_depth()
-    # mid_rgb = detector.get_mid_rgb()
+        arm_controller.safe_move_to_position(joint_positions_lower)
 
-    # Move around...
+        arm_controller.exec_gripper_cmd(0.03, 60)
 
-    # END STUDENT CODE
+        dropoff_transform = np.eye(4)
+        dropoff_transform[:3, 3] = dropoff_position
+        dropoff_transform[:3, 0] = np.array([1, 0, 0])
+        dropoff_transform[:3, 1] = np.array([0, 1, 0])
+        dropoff_transform[:3, 2] = np.array([0, 0, 1])
+
+        dropoff_joint_positions, _, success, message = inverse_kinematics.inverse(
+            dropoff_transform, arm_controller.get_positions(), "J_pseudo", 0.5
+        )
+        if not success:
+            print("Failed to compute IK for drop-off position:", message)
+
+        print("Moving to drop-off position")
+        arm_controller.safe_move_to_position(
+            np.array([0.35, 0, 0, -3.1415 / 2, 0, 3.1415 / 2, 3.1415 / 4])
+        )
+        print("Drop-off position reached")
+        arm_controller.open_gripper()
---
+++ lib/finalAssist.py
@@ -0,0 +1,96 @@
+from lib.IK_position_null import IK
+from lib.calculateFK import FK
+import numpy as np
+from math import pi
+
+class FinalAssist:
+    def __init__(self) :
+        self.dropT = np.array([[1,0,0,0.56],
+                                   [0,1,0,0.15],
+                                   [0,0,0,0.24],
+                                   [0,0,0,1]])
+
+    @staticmethod
+    def detectBlocks(self, arm, detector):
+        """
+        Block detection in order to find and transform 
+        block's position into world frame.
+
+        INPUTS:
+        arm - Arm controller object
+        detector - Detector object
+
+        OUTPUTS:
+        poses - Array of poses for each block in world frame
+        """
+
+        blocks = detector.get_detections()
+        currT0e = FK.forward(arm.get_positions())[1]
+        cameraToWorld = currT0e @ detector.get_H_ee_camera()
+        return [cameraToWorld @ pose for _,pose in blocks]
+    
+    @staticmethod
+    def getJointPos(transformation):
+        """
+        Uses IK class to find and return the joint configuration
+        for block pose
+
+        INPUTS:
+        transformation - 4x4 transformation matrix of a desired 
+        position in the world frame
+
+        OUTPUTS:
+        jointConfig - 1x7 array of the joint configurations
+        """
+        neutralPos = np.array([0,0,0,-pi/2,0,pi/2,pi/4])
+        
+        
+        jointConfig,_,success,_ =   IK.inverse(transformation,neutralPos, 'J_pseudo', 0.3)
+
+        if success:
+            return jointConfig
+        else:
+            return neutralPos
+
+    def getDropoffPos(self):
+        """
+        Gets the dropoff position based on previous
+        dropoffs
+        
+        INPUTS:
+
+        OUTPUTS:
+        jointConfig - 1x7 array of the joint configurations
+        """
+        neutralPos = np.array([0,0,0,-pi/2,0,pi/2,pi/4])
+        jointConfig,_,success,_ =   IK.inverse(self.dropT,neutralPos, 'J_pseudo', 0.3)
+
+        if success:
+            return jointConfig
+        else:
+            return neutralPos
+
+    @staticmethod
+    def pickUp(arm, blockPose):
+        """
+        Pickup function for static blocks. The arm
+        positions itself above the block and then lowers down
+        to pick it up.
+
+        INPUTS:
+        arm - Arm controller object
+        blockPose - 4x4 pose of block in world frame
+
+        OUTPUTS:
+        success - Boolean representing if pickup was successful or not
+        """
+        arm.open_gripper()
+        overBlockPose = np.copy(blockPose)
+        overBlockPose[2,3] += 0.225
+        jointConfig = FinalAssist.getJointPos(overBlockPose)
+        arm.safe_move_to_position(jointConfig)
+
+        jointConfig = FinalAssist.getJointPos(blockPose)
+        arm.safe_move_to_position(jointConfig)
+        arm.exec_gripper_cmd(0.03,60)
+        
