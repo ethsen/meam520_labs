57e67800944279b50633295712836bae004b1ce8
ethsen <ethsen@seas.upenn.edu>
2024-12-09 14:36:04 -0500

    ix/

--- lib/finalAssist.py
+++ lib/finalAssist.py
@@ -49,15 +49,15 @@ class FinalAssist:
                 blockDict[id] += world_pose
 
         # Compute the average pose for each block
-        poses = {id: blockDict[id] / 50 for id in blockDict}
-        print(poses.values())
+        poses = [blockDict[id] / 50 for id in blockDict]
+        print(poses)
         """
         #print("Pose in camera frame: ",np.round(pose,4))
         pose = cameraToWorld @ pose
         #print("Pose in world frame: ",np.round(pose,4))
         poses.append(pose)
         """
-        return poses.values()
+        return poses
     
     def getJointConfig(self,transformation, guess = np.array([-pi/8,0,0,-pi/2,0,pi/2,pi/4])):
         """
