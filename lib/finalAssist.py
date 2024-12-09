6b0f6b9f285741c2579e4e7a0f2bd7ea8e14d341
ethsen <ethansenatore@gmail.com>
2024-12-09 12:59:15 -0500

    please god

--- lib/finalAssist.py
+++ lib/finalAssist.py
@@ -123,14 +123,14 @@ class FinalAssist:
         self.arm.safe_move_to_position(jointConfig)
         pose = self.detectBlocks()[0]
         angle = np.arccos((np.trace(pose[:3,:3]) -1)/2)
-        print("Old Pose: ", pose,4)
+        print("Old Pose: ", np.round(pose,4))
 
         pose[:3,:3] = np.array([[np.cos(angle),-np.sin(angle),0],
                                 [np.sin(angle),np.cos(angle),0],
                                 [0,0,1]])
 
-        pose = pose @ np.array([[1,0,0,0],
-                                [0,-1,0,0],
+        pose = pose @ np.array([[-1,0,0,0],
+                                [0,1,0,0],
                                 [0,0,-1,0],
                                 [0,0,0,1]])
         print("Updated Pose: ", np.round(pose,4))        
